4ea0f3f66f159a7bdd157aeccbf59a9321627fae
A7med Fekry <a7medfekry@192.168.1.3>
2024-04-02 16:44:43 -0700

    fix dependency in baseAPIREQUESTe

--- Sources/Swiftaya/Network/ApiRequest/BaseAPIRequest.swift
+++ Sources/Swiftaya/Network/ApiRequest/BaseAPIRequest.swift
@@ -14,7 +14,7 @@ open class BaseAPIRequest<Response: Codable>: APIRequestProtocol {
     public let body: Data?
     public let headers: [String: String]?
     
-    init(baseUrl:String , path: String, method: String, body: Data? = nil, headers: [String: String]? = nil) {
+    public init(baseUrl:String , path: String, method: String, body: Data? = nil, headers: [String: String]? = nil) {
         self.path = path
         self.method = method
         self.body = body
